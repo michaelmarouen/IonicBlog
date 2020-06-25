ad6b89b5aaeafa09dd0fd130b5f3c5e91403b20c
MMC <michaelconfortini@gmail.com>
2020-06-25 12:39:23 +0200

    Insertion des posts via le service dans la page Blog

--- src/app/components/blog-page/blog-page.component.ts
+++ src/app/components/blog-page/blog-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BlogService} from 'src/app/service/blog/blog.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-blog-page',
@@ -7,13 +8,15 @@ import { BlogService} from 'src/app/service/blog/blog.service';
   styleUrls: ['./blog-page.component.scss']
 })
 export class BlogPageComponent implements OnInit {
-  reponse;
+
+  allPosts$ : Observable<any>;  
   constructor(
     private _apiBlogService : BlogService
   ) { }
 
   ngOnInit(): void {
-    this.reponse = this._apiBlogService.load()
+    this._apiBlogService.load()
+    this.allPosts$ = this._apiBlogService.data$;
   }
 
   detailArticle(){
