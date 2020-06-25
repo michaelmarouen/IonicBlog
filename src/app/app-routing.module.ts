6b048dd26edae57243ef87af23b1d80f26310d9d
MMC <michaelconfortini@gmail.com>
2020-06-25 12:41:54 +0200

    Mise à jour des routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BlogPageComponent } from './components/blog-page/blog-page.component';
+import { ItemPageComponent } from './components/item-page/item-page.component';
+import { EditPageComponent } from './components/edit-page/edit-page.component';
 
 
 const routes: Routes = [
-  {path:'blog', component: BlogPageComponent},
+  {path:'blog', 
+      children:[
+        {path:'', component: BlogPageComponent},
+        {path:':id', component: ItemPageComponent},
+        {path:':id/edit', component: EditPageComponent}
+      ]
+  },
   {
     path: '',
     redirectTo: 'blog',
