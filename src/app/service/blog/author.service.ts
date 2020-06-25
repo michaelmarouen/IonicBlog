280e1c189f26064e42c608e7b51933dc30b0b1f8
MMC <michaelconfortini@gmail.com>
2020-06-26 01:22:03 +0200

    Récuparation auteur par id

--- src/app/service/blog/author.service.ts
+++ src/app/service/blog/author.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { first } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorService {
 
-  constructor() { }
+  constructor(
+      private http: HttpClient
+  ) { }
+
+  async getAuthorById(id:string){
+    const url = 'https://jsonplaceholder.typicode.com/users?id='+id
+    return await this.http.get(url).pipe(first()).toPromise();    
+  }
 }
