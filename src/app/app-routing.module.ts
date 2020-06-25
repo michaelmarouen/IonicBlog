89a6e535d9e93d3cfe5c5d0d3a3f0464966a0386
MMC <michaelconfortini@gmail.com>
2020-06-25 11:54:22 +0200

    Routing + template blog

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogPageComponent } from './components/blog-page/blog-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'blog', component: BlogPageComponent},
+  {
+    path: '',
+    redirectTo: 'blog',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
