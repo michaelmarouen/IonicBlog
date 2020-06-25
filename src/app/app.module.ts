cfc1052c6aabfc1a4708137d31444cd74140815a
MMC <michaelconfortini@gmail.com>
2020-06-25 11:48:32 +0200

    Création layout Blog et item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IonicModule } from '@ionic/angular';
+import { BlogPageComponent } from './components/blog-page/blog-page.component';
+import { ItemPageComponent } from './components/item-page/item-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogPageComponent,
+    ItemPageComponent
   ],
   imports: [
     BrowserModule,
